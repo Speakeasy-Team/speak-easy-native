9d9e7d180ee56584fb65b017d701a7d604547d72
Elijah Kim <kimelijah90@gmail.com>
2016-01-21 14:31:31 -0500

    Render data from json

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from "react-native";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import Root from "./containers/Root";
+import data from "./sample_data";
 
-const reducer = () => {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADED':
+      return data
+    default:
+      return state
+  }
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, { speakEasies: [] });
 
 class EasySpeak extends Component {
   render() {
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react-native";
 import HomeScreen from "../components/HomeScreen";
+import { connect } from 'react-redux'
 
 class Root extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch({ type: 'LOADED' });
+  }
+
   render() {
     return (
       <HomeScreen />
@@ -9,4 +16,10 @@ class Root extends Component {
   }
 }
 
-export default Root;
+const select = (state) => {
+  return {
+    speakEasies: state.speakEasies
+  }
+}
+
+export default connect(select)(Root);
